aa2d762942873c1942bc7f7c8f9d197d9f569b5f
Geoffrey Sneddon <geoffers@gmail.com>
2007-12-15 22:12:14 +0000

    Add proper content-type headers. Make XMLHttpRequest synchronous.

--- tests/response/response.py
+++ tests/response/response.py
@@ -16,83 +16,87 @@ import SimpleHTTPServer
 
 class HTTPParsingReponseTestServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
-    """Simple HTTP request handler with GET and HEAD commands.
+	"""Simple HTTP request handler with GET and HEAD commands.
 
-    This serves files from the current directory and any of its
-    subdirectories.
+	This serves files from the current directory and any of its
+	subdirectories.
 
-    The GET and HEAD requests are identical except that the HEAD
-    request omits the actual contents of the file.
+	The GET and HEAD requests are identical except that the HEAD
+	request omits the actual contents of the file.
 
-    """
+	"""
 
-    server_version = "HTTPParsingReponseTestServer/" + __version__
+	server_version = "HTTPParsingReponseTestServer/" + __version__
 
-    def do_HEAD(self):
-        """Serve a HEAD request."""
-        f = self.send_head()
-        if f:
-            if self.path.startswith("/tests"):
-                data = f.read()
-                LFLF = data.find("\n\n")
-                LFCRLF = data.find("\n\r\n")
-                if (-1 < LFLF < LFCRLF):
-                    self.wfile.write(data[:LFLF + 2])
-                elif (LFCRLF > -1):
-                    self.wfile.write(data[:LFCRLF + 3])
-                else:
-                    self.send_error(500, "Internal Server Error")
-            f.close()
+	def do_HEAD(self):
+		"""Serve a HEAD request."""
+		f = self.send_head()
+		if f:
+			if self.path.startswith("/tests"):
+				data = f.read()
+				LFLF = data.find("\n\n")
+				LFCRLF = data.find("\n\r\n")
+				if (-1 < LFLF < LFCRLF):
+					self.wfile.write(data[:LFLF + 2])
+				elif (LFCRLF > -1):
+					self.wfile.write(data[:LFCRLF + 3])
+				else:
+					self.send_error(500, "Internal Server Error")
+			f.close()
 
-    def send_head(self):
-        """Common code for GET and HEAD commands.
+	def send_head(self):
+		"""Common code for GET and HEAD commands.
 
-        Return value is either a file object (which has to be copied
-        to the outputfile by the caller unless the command was HEAD,
-        and must be closed by the caller under all circumstances), or
-        None, in which case the caller has nothing further to do.
+		Return value is either a file object (which has to be copied
+		to the outputfile by the caller unless the command was HEAD,
+		and must be closed by the caller under all circumstances), or
+		None, in which case the caller has nothing further to do.
 
-        """
-        if self.request_version == 'HTTP/0.9':
-            self.send_error(505, "HTTP Version Not Supported")
-            return None
-        path = self.translate_path(self.path)
-        f = None
-        if os.path.isdir(path):
-            if not self.path.endswith('/'):
-                # redirect browser - doing basically what apache does
-                self.send_response(301)
-                self.send_header("Location", self.path + "/")
-                self.end_headers()
-                return None
-            elif os.path.exists(os.path.join(path, "index.html")):
-                path = index
-            else:
-                f = self.list_directory(path)
-                self.copyfile(f, self.wfile)
-                return None
-        try:
-            f = open(path, "r")
-        except IOError:
-            self.send_error(404, "File not found")
-            return None
-        if not self.path.startswith("/tests"):
-            self.send_response(200)
-            if self.path == "/runtests.html":
-                self.send_header("Content-type", "text/html")
-            else:
-                self.send_header("Content-type", "text/plain")
-            fs = os.fstat(f.fileno())
-            self.send_header("Content-Length", str(fs[6]))
-            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-            self.end_headers()
-        return f
+		"""
+		if self.request_version == 'HTTP/0.9':
+			self.send_error(505, "HTTP Version Not Supported")
+			return None
+		path = self.translate_path(self.path)
+		f = None
+		if os.path.isdir(path):
+			if not self.path.endswith('/'):
+				# redirect browser - doing basically what apache does
+				self.send_response(301)
+				self.send_header("Location", self.path + "/")
+				self.end_headers()
+				return None
+			elif os.path.exists(os.path.join(path, "index.html")):
+				path = index
+			else:
+				f = self.list_directory(path)
+				self.copyfile(f, self.wfile)
+				return None
+		try:
+			f = open(path, "r")
+		except IOError:
+			self.send_error(404, "File not found")
+			return None
+		if not self.path.startswith("/tests"):
+			self.send_response(200)
+			if self.path == "/runtests.html":
+				self.send_header("Content-type", "text/html;charset=utf-8")
+			elif self.path == "/runtests.js":
+				self.send_header("Content-type", "text/javascript;charset=utf-8")
+			elif self.path == "/runtests.css":
+				self.send_header("Content-type", "text/css;charset=utf-8")
+			else:
+				self.send_header("Content-type", "text/plain")
+			fs = os.fstat(f.fileno())
+			self.send_header("Content-Length", str(fs[6]))
+			self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+			self.end_headers()
+		return f
 
 
 def test(HandlerClass = HTTPParsingReponseTestServer,
-         ServerClass = BaseHTTPServer.HTTPServer):
-    BaseHTTPServer.test(HandlerClass, ServerClass)
+		 ServerClass = BaseHTTPServer.HTTPServer):
+	BaseHTTPServer.test(HandlerClass, ServerClass)
 
 
 if __name__ == '__main__':
-    test()
+	test()
--- tests/response/runtests.js
+++ tests/response/runtests.js
@@ -94,13 +94,12 @@ function run_test(test)
 			{
 				return;
 			}
-			log('Test: ' + test);
 			ok = compare(expected, test_xhr);
 		};
-		test_xhr.open('GET', 'tests/' + test + '.http');
+		test_xhr.open('GET', 'tests/' + test + '.http', false);
 		test_xhr.send(null);
 	}
-	expected_xhr.open('GET', 'expected/' + test + '.expected');
+	expected_xhr.open('GET', 'expected/' + test + '.expected', false);
 	expected_xhr.send(null);
 	return ok;
 }
@@ -121,7 +120,8 @@ function start()
 	var passes = 0;
 	for (var i = 0; i < test_names.length; i++)
 	{
-		update_status('Running... (test ' + (i + 1) + '/' + test_names.length + ', currently passed ' + passes + ')');
+		update_status('Running… (test ' + (i + 1) + '/' + test_names.length + ', currently passed ' + passes + ')');
+		log('Test ' + (i + 1) + ': ' + test_names[i]);
 		var pass = run_test(test_names[i]);
 		test_result(test_names[i], pass);
 		if (pass)
